8f4d46fd63052fe01ca634123c40052cde18a5d1
SuNetVerseWolk <volk333333333@gmail.com>
2024-12-15 20:35:23 +0300

    fix

--- front/src/app/(auth)/error/page.tsx
+++ front/src/app/(auth)/error/page.tsx
@@ -7,7 +7,7 @@ export default function page() {
 			<h1 className='text-center text-4xl text-cats-lightCoral'>
 				Ошибка сервера
 			</h1>
-			<p className='text-center md:text-s'>
+			<p>
 				Ошибка конфигурации сервера.
 			</p>
 			<p>
