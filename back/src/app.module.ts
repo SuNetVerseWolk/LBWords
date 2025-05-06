0de639debbea16699994af39ad52ef68ea998588
SuNetVerseWolk <volk333333333@gmail.com>
2025-05-06 21:22:00 +0300

    set-up

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ProfilesModule } from './profiles/profiles.module';
+import { DictionaryModule } from './dictionary/dictionary.module';
 
 @Module({
-  imports: [],
+  imports: [BooksModule, PrismaModule, ProfilesModule, DictionaryModule],
   controllers: [AppController],
   providers: [AppService],
 })
